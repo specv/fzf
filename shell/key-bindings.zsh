daca3d2cd83b5e1cf710c6d5f56b3cfecb8f87d3
specv <61646426+specv@users.noreply.github.com>
2022-04-22 09:54:14 +0800

    Use `fd` instead of `find`

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -40,10 +40,7 @@ fi
 
 # CTRL-T - Paste the selected file path(s) into the command line
 __fsel() {
-  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type f -print \
-    -o -type d -print \
-    -o -type l -print 2> /dev/null | cut -b3-"}"
+  local cmd="${FZF_CTRL_T_COMMAND:-"command fd -H -t f 2> /dev/null | cut -b3-"}"
   setopt localoptions pipefail no_aliases 2> /dev/null
   local item
   local accept=0
@@ -86,8 +83,7 @@ bindkey -M viins '^O' fzf-file-widget
 
 # ALT-C - cd into the selected directory
 fzf-cd-widget() {
-  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type d -print 2> /dev/null | cut -b3-"}"
+  local cmd="${FZF_ALT_C_COMMAND:-"command fd -H -t d 2> /dev/null | cut -b3-"}"
   setopt localoptions pipefail no_aliases 2> /dev/null
   local dir
   local item
